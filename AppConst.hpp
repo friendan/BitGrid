90258b2e87872c682d6e8d42e2cd625738df049c
friendan <407583729@qq.com>
2026-04-23 23:45:27 +0800

    init

--- AppConst.hpp
+++ AppConst.hpp
@@ -6,7 +6,7 @@ namespace AppConst
 	// 二进制值(0~15) → 16进制字符
 	constexpr char BinTohex[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
-	constexpr int BORDER_LINE_OFFSET = 2;	// 表示：从顶部/底部/左侧/右侧 数第几条线
+	constexpr int BORDER_LINE_OFFSET = 5;	// 表示：从顶部/底部/左侧/右侧 数第几条线
 	constexpr int BORDER_LINE_COUNT  = 3;	// 表示：从顶部/底部/左侧/右侧 依次画N条线
 	constexpr COLORREF BORDER_COLOR  = RGB(0, 0, 0);	// 黑色边框
 	// constexpr COLORREF BORDER_COLOR  = RGB(255, 0, 0);	// 红色边框（旧）
